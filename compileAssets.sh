33949d9384f9bc58c17fbf24b7f39aca3e5c2c4e
IainWinter <iainwinter1@gmail.com>
2023-12-28 03:14:14 -0500

    imgui patch file for srgb hack

--- compileAssets.sh
+++ compileAssets.sh
@@ -2,10 +2,10 @@ cd "${0%/*}"
 
 mkdir ./build/shaders
 
-glslc .\src_assets\shaders\particle\particle.vert -o .\build\shaders\particle_vert.spv
-glslc .\src_assets\shaders\particle\particle.frag -o .\build\shaders\particle_frag.spv
+glslc ./src_assets/shaders/particle/particle.vert -o ./build/shaders/particle_vert.spv
+glslc ./src_assets/shaders/particle/particle.frag -o ./build/shaders/particle_frag.spv
 
-glslc .\src_assets\shaders\line\line.vert -o .\build\shaders\line_vert.spv
-glslc .\src_assets\shaders\line\line.frag -o .\build\shaders\line_frag.spv
+glslc ./src_assets/shaders/line/line.vert -o ./build/shaders/line_vert.spv
+glslc ./src_assets/shaders/line/line.frag -o ./build/shaders/line_frag.spv
 
 ./asset_compiler/build/lithAssetCompiler ./assets.input ./build/assets.bin
---
+++ subprojects/packagefiles/imgui_srgb/gen_patch.sh
@@ -0,0 +1,2 @@
+cd "${0%/*}"
+diff -u ../../imgui-1.89.9/backends/imgui_impl_vulkan.cpp ./imgui_impl_vulkan.cpp > ./imgui_srgb.patch.new
